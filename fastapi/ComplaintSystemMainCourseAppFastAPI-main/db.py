d13ce8b3dc06785152a4c56c11f8f6e924dc41c8
virmos <hadonggiang1810@gmail.com>
2022-12-29 08:17:40 +0700

    add lectures

--- fastapi/ComplaintSystemMainCourseAppFastAPI-main/db.py
+++ fastapi/ComplaintSystemMainCourseAppFastAPI-main/db.py
@@ -2,6 +2,6 @@ import databases
 import sqlalchemy
 from decouple import config
 
-DATABASE_URL = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:5433/complaints"
+DATABASE_URL = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:5432/complaints"
 database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
